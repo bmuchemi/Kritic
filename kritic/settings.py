302e3bd07ca68f1eac2ceb321c2e305b13e73c7a
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2022-06-14 18:30:48 +0300

    change database settings to use postgres

--- kritic/settings.py
+++ kritic/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'kritic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kritic',
+        'USER': 'bmuchemi',
+    'PASSWORD':'BENJA9262',
     }
 }
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
